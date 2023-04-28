38f29318e0cef04512df9628c1a55b805055bb27
mtgibson8 <126990635+mtgibson8@users.noreply.github.com>
2023-04-28 10:38:39 -0500

    psuedo coded everything I will need to do

---
+++ db/schema.sql
@@ -0,0 +1,5 @@
+--  this file will contain the SQL queries to create the necessary tables in my database.
+
+-- I need to define the schema for my database in schema.sql. I need tables for departments, roles, and employees, as well as for foreign key constraints.
+
+-- need to connect to my mysql server and run the schema.sql file to create the tables.
---
+++ index.js
@@ -0,0 +1,8 @@
+// need to set up  database connection in index.js using mysql module.
+
+// need to create a function to display the main menu using Inquirer. This function should prompt the user with the available options (view all departments, view all roles, etc.) and call the corresponding functions based on user input.
+
+
+// functions I need to make:    one to view all departments, which retrieves department data from the database and displays it as a formatted table.    one to view all roles, which retrieves role data from the database and displays it as a formatted table.   one to  view all employees, which retrieves employee data from the database and displays it as a formatted table.   one to add a department, which prompts the user for the department name and inserts it into the database.   one to add a role, which prompts the user for the role name, salary, and department and inserts it into the database.  one to add an employee, which prompts the user for the employee's first name, last name, role, and manager and inserts it into the database.  one to update an employee role, which prompts the user to select an employee and their new role and updates the database accordingly.
+
+// need to start the application by calling the main menu function.
