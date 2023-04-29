fdca6b8c2f34cafed9e67b9848a0bb906f1823d3
mtgibson8 <126990635+mtgibson8@users.noreply.github.com>
2023-04-29 17:07:37 -0500

    "aaded rough draft of querires.js file with all my functions"

--- assets/queries.js
+++ assets/queries.js
@@ -1,3 +1,48 @@
-const consoleTable = require('console.table');
+
 const promiseConnection = require('./connector');
 
+
+const viewAllEmployees = async () => {
+  const [rows] = await promiseConnection.query('SELECT * FROM employee');
+  console.table(rows);
+};
+
+const addAnEmployee = async (employeeData) => {
+  await promiseConnection.query('INSERT INTO employee SET ?', employeeData);
+  console.log(`Employee ${employeeData.firstName} ${employeeData.lastName} added successfully.`);
+};
+
+const updateEmployee = async (employeeData) => {
+  await promiseConnection.query('UPDATE employee SET role_id = ? WHERE id = ?', [employeeData.role_id, employeeData.employee_id]);
+  console.log(`Employee ${employeeData.firstName} ${employeeData.lastName} updated successfully.`);
+};
+
+const viewAllRoles = async () => {
+  const [rows] = await promiseConnection.query('SELECT * FROM role');
+  console.table(rows);
+};
+
+const addARole = async (roleData) => {
+  await promiseConnection.query('INSERT INTO role SET ?', roleData);
+  console.log(`Role ${roleData.title} added successfully.`);
+};
+
+const viewAllDepartments = async () => {
+  const [rows] = await promiseConnection.query('SELECT * FROM department');
+  console.table(rows);
+};
+
+const addADepartment = async (departmentData) => {
+  await promiseConnection.query('INSERT INTO department SET ?', departmentData);
+  console.log(`Department ${departmentData.name} added successfully.`);
+};
+
+module.exports = {
+  viewAllEmployees,
+  addAnEmployee,
+  updateEmployee,
+  viewAllRoles,
+  addARole,
+  viewAllDepartments,
+  addADepartment
+};
