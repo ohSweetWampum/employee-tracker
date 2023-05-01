9a565fe9a01d5b18de3a7bb9284035400f615c60
mtgibson8 <126990635+mtgibson8@users.noreply.github.com>
2023-05-01 09:49:00 -0500

    rewrote most of the index.js file again, as I was getting so many errors with the last version, this method of running the function seems to work for now

--- index.js
+++ index.js
@@ -1,57 +1,46 @@
 
-const inquirer = require('inquirer'); 
 const prompts = require('./assets/prompts');
 const queries = require('./assets/queries');
 
-//main menu function
-function mainMenuFunction() {
-  inquirer.prompt(prompts.mainMenu()) 
-    .then((responses) => {
-      switch (responses.menuSelections) {
-        case "View all employees": functionDirectory['View all employees'](); 
-        break;
-        case "Add an employee": functionDirectory['Add an employee'](); 
-        break;
-        case "Update an employee role": functionDirectory['Update an employee role'](); 
-        break;
-        case "View all roles": functionDirectory['View all roles']();
-        break;
-        case "Add a role": functionDirectory['Add a role'](); 
-        break;
-        case "View all departments": functionDirectory['View all departments'](); 
-        break;
-        case "Add department": functionDirectory['Add department']();
-        break;
-        case "Quit": functionDirectory['Quit'](); 
-        break;
-      }
-    })
-}
-//"look up table" that directs to a particular function in the queries.js file, based off the users selection, the specific function is then executed
+
+// mainMenuFunction();
 const functionDirectory = {
-  'View all employees': queries.viewAllEmployees,
-  'Add an employee': async () => {
+  "View all employees": queries.viewAllEmployees,
+  "Add an employee": async () => {
     const employeeData = await prompts.addAnEmployee();
     await queries.addAnEmployee(employeeData);
   },
-  'Update an employee role': async () => {
+  "Update an employee role": async () => {
     const updateData = await prompts.updateEmployee();
     await queries.updateEmployee(updateData);
   },
-  'View all roles': queries.viewAllRoles,
-  'Add a role': async () => {
+  "View all roles": queries.viewAllRoles,
+  "Add a role": async () => {
     const roleData = await prompts.addARole();
     await queries.addARole(roleData);
   },
-  'View all departments': queries.viewAllDepartments,
-  'Add department': async () => {
+  "View all departments": queries.viewAllDepartments,
+  "Add department": async () => {
     const departmentData = await prompts.addADepartment();
     await queries.addADepartment(departmentData);
   },
-  'Quit': async () => {
+  "Quit": async () => {
     console.log("Exiting...");
     process.exit();
-  }
+  },
 };
+
+async function mainMenuFunction() {
+  try {
+    const responses = await prompts.mainMenu();
+    const selectedFunction = functionDirectory[responses.menuSelections];
+    if (typeof selectedFunction === 'function') {
+      await selectedFunction();
+    }
+    mainMenuFunction(); 
+  } catch (error) {
+    console.error('Error:', error);
+  }
+}
 
 mainMenuFunction();
