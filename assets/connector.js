adede9e76008fdc0f43391b0969b86f9ebc9891a
mtgibson8 <126990635+mtgibson8@users.noreply.github.com>
2023-05-02 00:14:01 -0500

    deleted password

--- assets/connector.js
+++ assets/connector.js
@@ -5,7 +5,7 @@ const mysql2 = require("mysql2");
 const db = mysql2.createConnection({
   host: "localhost", // hostname of the database server
   user: "root", //  MySQL user to authenticate as
-  password: "NewPath88!", // My password for MySQL
+  password: "", // My password for MySQL
   database: "complete_company_db", // database to use for the connection
   multipleStatements: true,
 });
