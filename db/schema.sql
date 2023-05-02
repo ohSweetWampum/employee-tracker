753492c3fa213061dd5d125e481771bb1ce990a2
mtgibson8 <126990635+mtgibson8@users.noreply.github.com>
2023-05-01 21:39:51 -0500

    deleted seedDatabase function, completly useless

--- db/schema.sql
+++ db/schema.sql
@@ -1,83 +1,42 @@
 
 
--- DROP DATABASE IF EXISTS complete_company_db;
--- CREATE DATABASE complete_company_db;
-
--- USE complete_company_db;
-
-
-
--- CREATE TABLE department (
---   id INT NOT NULL AUTO_INCREMENT ,
---   department_name VARCHAR(30) ,
---   PRIMARY KEY (id)
--- );
-
-
--- CREATE TABLE roles (
---     id INT NOT NULL AUTO_INCREMENT,
---     title VARCHAR(30),
---     salary DECIMAL (10,2),
---     department_id INT,
---     Primary KEY (id),
---     FOREIGN KEY (department_id )
---     REFERENCES department(id)
---     ON DELETE SET NULL
--- );
-
-
-
-
--- CREATE TABLE employees (
---     id INT NOT NULL AUTO_INCREMENT,
---     first_name VARCHAR(30),
---     last_name VARCHAR(30),
---     roles_id INT,
---     manager_id INT,
---     PRIMARY KEY (id),
---     FOREIGN KEY (roles_id)
---     REFERENCES roles(id)
---     ON DELETE SET NULL,
---     FOREIGN KEY (manager_id)
---     REFERENCES employees(id)
---     ON DELETE SET NULL
--- );
-
-
 
 DROP DATABASE IF EXISTS complete_company_db;
 CREATE DATABASE complete_company_db;
 
 USE complete_company_db;
-
+DROP TABLE IF EXISTS department;
 CREATE TABLE department (
   id INT NOT NULL AUTO_INCREMENT PRIMARY KEY ,
   department_name VARCHAR(30)  
  
 );
-
+DROP TABLE IF EXISTS roles;
 CREATE TABLE roles (
     id INT NOT NULL AUTO_INCREMENT PRIMARY KEY,
     title VARCHAR(30),
-    salary DECIMAL  (10,2) ,
     department_id INT,
+    salary DECIMAL  (10,2) ,
+    
     FOREIGN KEY (department_id )
     REFERENCES department(id)
     ON DELETE SET NULL
 );
-
+DROP TABLE IF EXISTS employees;
 CREATE TABLE employees (
     id INT NOT NULL AUTO_INCREMENT PRIMARY KEY,
     first_name VARCHAR(30) NOT NULL,
     last_name VARCHAR(30) NOT NULL,
     roles_id INT,
-    manager_id INT,
+   
     FOREIGN KEY (roles_id)
     REFERENCES roles(id)
     ON DELETE SET NULL,
+    manager_id INT,
     FOREIGN KEY (manager_id)
     REFERENCES employees(id)
     ON DELETE SET NULL
+    
 );
 
 
